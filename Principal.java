ca55373d9eec5c2e4c20c930c7abcb2490c1a583
BelenTM <b.tejeramaicas@edu.gva.es>
2024-11-08 20:07:33 +0100

    Saludo.java y Despedida.java modificado. Añadido Principal.java

---
+++ Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main (String[] args){
+        Saludo saludo = new Saludo();
+        Despedida despedida = new Despedida();
+
+        saludo.saludar();
+        despedida.despedir();
+    }
+    
+}

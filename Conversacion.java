9f4002b48cdadc92745a5f7b6471d2619262d609
BelenTM <b.tejeramaicas@edu.gva.es>
2024-11-12 14:43:46 +0100

    Archivo Conversacion.java creado

---
+++ Conversacion.java
@@ -0,0 +1,15 @@
+public class Conversacion {
+    private Saludo saludo;
+    private Despedida despedida;
+
+    public Conversacion(){
+        saludo = new Saludo();
+        despedida = new Despedida();
+    }
+
+    public void iniciarConversacion(){
+        saludo.saludar();
+        despedida.despedir();
+    }
+    
+}

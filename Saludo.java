29060f5c2183b5e58f93949cfc4a19ae68c5b4df
BelenTM <b.tejeramaicas@edu.gva.es>
2024-11-22 17:41:51 +0100

    Añadido nuevo print en Saludo.java

--- Saludo.java
+++ Saludo.java
@@ -5,6 +5,7 @@ public class Saludo{
         System.out.print("Dime tu nombre");
         String nombre = scan.nextLine();
         System.out.println("Hola " + nombre + " ¡Hace mucho que no nos vemos!");
+        System.out.println("Esto es una prueba de sincronizacion");
     }
 
     public void saludarConEdad(){

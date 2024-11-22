8653826c757ccac85989cbb504c72734f3c64a5d
BelenTM <b.tejeramaicas@edu.gva.es>
2024-11-22 17:29:13 +0100

    Añadido nuevo print metodo Saludar()

--- Saludo.java
+++ Saludo.java
@@ -4,7 +4,7 @@ public class Saludo{
         Scanner scan = new Scanner(System.in);
         System.out.print("Dime tu nombre");
         String nombre = scan.nextLine();
-        System.out.println("Hola " + nombre);
+        System.out.println("Hola " + nombre + " ¡Hace mucho que no nos vemos!");
     }
 
     public void saludarConEdad(){

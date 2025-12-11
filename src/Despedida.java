a679568533689c52d85a35e773405d397e2f0f1d
BelenTM <b.tejeramaicas@edu.gva.es>
2025-12-11 12:15:31 +0100

    Clase Saludo.java, Despedida.java y Principal.java modificado

--- src/Despedida.java
+++ src/Despedida.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Despedida {
+    public void despedir() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dime tu nombre: ");
+        String nombre = scan.nextLine();
+        System.out.println("Hasta luego " + nombre);
+    }
 }
